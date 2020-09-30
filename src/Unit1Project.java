ed03decd8c92dbf62abef173e80e2569da69f9f7
dchoi2145 <63204411+dchoi2145@users.noreply.github.com>
2020-09-29 21:37:03 -0400

    Add files via upload

---
+++ src/Unit1Project.java
@@ -0,0 +1,10 @@
+
+public class Unit1Project {
+	public static void main(String[] args) {
+		System.out.println("Email: choidh23@sharkmail.com"); //print email
+		System.out.println("Birthday: 10072004"); //print password
+		System.out.println("*** This temporary password is very insecure. Please change it immeidately! ***"); //prints prompt
+
+	}
+
+}
