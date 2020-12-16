8711a5c69a26140980cf9cd686db868f6fdf06d5
dchoi2145 <63204411+dchoi2145@users.noreply.github.com>
2020-12-16 14:56:50 -0500

    Add files via upload

---
+++ Prom Ticket/src/PromTicketTest.java
@@ -0,0 +1,10 @@
+
+public class PromTicketTest {
+
+	public static void main(String[] args) {
+		PromTicket prom = new PromTicket("David Choi", 111111, "freshman", 4, 0);
+		prom.printAll();
+
+	}
+
+}
